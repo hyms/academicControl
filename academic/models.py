c7e5335864d430f2ad48ded727e17e954bd8df0d
ipxserver <hcortez@ipxserver.com>
2013-08-17 22:57:03 -0400

    update to eclipse proyect

---
+++ academic/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class user
+
+class carrera
+
+class materia
+
+class docencia
+
+class asignar
+
+class notas
+
+class horario
+
+class transaccion
